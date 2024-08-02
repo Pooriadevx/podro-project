fdcf7a7c45c9dafeb60046cb9ad4cb3f8694dcf0
Pooriadevx <pooria.b1378@gmail.com>
2024-08-02 23:11:25 +0330

    feat: Created loading progress

---
+++ src/components/circularProgress/circularProgress.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./style.module.scss";
+
+const CircularProgress: React.FC = () => {
+  return <div className={classes.loading} />;
+};
+
+export default CircularProgress;
