5aad23afbf002da9514d43d6584960ce32f17d68
Pooriadevx <pooria.b1378@gmail.com>
2024-08-02 23:03:06 +0330

    feat: Created global theme

---
+++ src/theme/global.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const theme: DefaultTheme = {
+  primaryColor: "#007bff",
+  secondaryColor: "#6c757d",
+  fontFamily: "sans-serif",
+  className: "App",
+};
